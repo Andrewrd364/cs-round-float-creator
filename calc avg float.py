db6fb42bfe0a7a8250cec5826e58090faccdb3b0
andre <andrey.liskov03@mail.ru>
2023-10-06 23:54:19 +0500

    performance and accuracy improvements

--- calc avg float.py
+++ calc avg float.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 import struct
+import time
 
 
 def get_ieee754(value: float) -> float:
@@ -8,19 +9,23 @@ def get_ieee754(value: float) -> float:
     return result
 
 
+# Change expected float value of the skin here
 exp_float = 0.5
+# Change float caps of the expected skin here
 float_caps = [0.06, 0.8]
 
-lower_bound = exp_float
-higher_bound = exp_float
-while get_ieee754(lower_bound) == exp_float:
-    lower_bound -= 0.000000000001
-lower_bound += 0.00000000001
-while get_ieee754(higher_bound) == exp_float:
-    higher_bound += 0.000000000001
-higher_bound -= 0.00000000001
+float_caps[0], float_caps[1] = get_ieee754(float_caps[0]), get_ieee754(float_caps[1])
+difference = get_ieee754(float_caps[1] - float_caps[0])
+exp_float_in_caps = get_ieee754((exp_float - float_caps[0]) / difference)
 
-difference = float_caps[1] - float_caps[0]
+lower_bound = higher_bound = exp_float_in_caps
+
+while get_ieee754(lower_bound * difference + float_caps[0]) == exp_float:
+    lower_bound -= 0.000000000001
+lower_bound += 0.00000001
+while get_ieee754(higher_bound * difference + float_caps[0]) == exp_float:
+    higher_bound += 0.000000000001
+higher_bound -= 0.00000001
 
 
 def read_text():
@@ -29,7 +34,7 @@ def read_text():
         for i in file.readlines():
             if i.find("Float:") != -1:
                 filtered_text.append(get_ieee754(float(i.replace("Float: ", "").replace("\n", ""))))
-            if len(filtered_text) == 34:
+            if len(filtered_text) == 33:
                 break
 
     with open('input.txt', 'w') as file:
@@ -38,20 +43,24 @@ def read_text():
     return filtered_text
 
 
-def is_within_bounds(combination):
-    outcome = (difference * get_ieee754(sum(combination) / 10)) + float_caps[0]
-    return lower_bound < outcome < higher_bound
-
-
 def main():
-    valid_combinations = filter(is_within_bounds, combinations(read_text(), 10))
+    time_start = time.perf_counter()
 
-    for combination in valid_combinations:
-        avg = sum(combination) / 10
-        outcome = ((float_caps[1] - float_caps[0]) * avg) + float_caps[0]
-        print(outcome)
-        result_str = " or ".join([f'match(float, "{str(fv)[2:7]}")>=1' for fv in combination])
-        print(result_str)
+    float_list = read_text()
+    combs = combinations(float_list, 10)
+
+    for i in combs:
+        outcome = sum(i) / 10
+        if lower_bound < outcome < higher_bound:
+            print(outcome * difference + float_caps[0])
+            result_str = " or ".join([f'match(float, "{str(fv)[2:8]}")>=1' for fv in i])
+            print(result_str)
+            break
+
+    time_end = time.perf_counter()
+    time_duration = time_end - time_start
+
+    print(f'Took {time_duration:.3f} seconds')
 
 
 if __name__ == '__main__':
